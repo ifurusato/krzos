d9b9ffb18ff7dc73066a1c7ad6036fa4a5f91235
ifurusato <ichiro.furusato@gmail.com>
2025-12-31 20:07:53 +1300

    add temporary commmand line utility

---
+++ stm.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020-2026 by Ichiro Furusato. All rights reserved. This file is part
+# of the Robot Operating System project, released under the MIT License. Please
+# see the LICENSE file included as part of this package.
+#
+# author:   Ichiro Furusato
+# created:  2025-11-16
+# modified: 2025-12-03
+
+from hardware.i2c_master import I2CMaster
+
+def main():
+    try:
+        NAME= 'pico'
+        master = I2CMaster(NAME, i2c_address=0x45, timeset=True)
+        master.enable()
+        while True:
+            user_msg = input('Enter command string to send ("quit" to exit): ')
+            if user_msg.strip().lower() == 'quit':
+                break
+            if len(user_msg) == 0:
+                continue
+            data_request = user_msg.startswith('!')
+            if data_request:
+                user_msg = user_msg[1:]
+                response = master.send_data_request(user_msg)
+                print('data response: {}'.format(response))
+            else:
+                response = master.send_request(user_msg)
+                print('response: {}'.format(response))
+    except KeyboardInterrupt:
+        print('Ctrl-C caught, exiting…')
+    except Exception as e:
+        print('error: {}'.format(e))
+
+if __name__ == '__main__':
+    main()
+
+#EOF
