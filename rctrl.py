b1bfef1df9fa9a117c2562096023c262cc031d6e
ifurusato <ichiro.furusato@gmail.com>
2026-02-04 12:23:04 +1300

    controller for radiozoa

---
+++ rctrl.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020-2026 by Ichiro Furusato. All rights reserved. This file is part
+# of the Robot Operating System project, released under the MIT License. Please
+# see the LICENSE file included as part of this package.
+#
+# author:   Ichiro Furusato
+# created:  2025-11-16
+# modified: 2026-01-03
+
+from hardware.stm32_controller import Stm32Controller
+
+I2C_ADDRESS = 0x47
+ALWAYS_DATA_REQUEST = False
+
+def main():
+    try:
+#       master = I2CMaster(Stm32Controller.NAME, i2c_address=0x45, timeset=True)
+        master = Stm32Controller(i2c_address=I2C_ADDRESS)
+        master.enable()
+        while True:
+            user_msg = input('Enter command string to send ("quit" to exit): ')
+            if user_msg.strip().lower() == 'quit':
+                break
+            print('user msg: {}'.format(user_msg))
+            if len(user_msg) == 0:
+                continue
+            data_request = user_msg.startswith('!')
+            if data_request:
+                user_msg = user_msg[1:]
+            if ALWAYS_DATA_REQUEST or data_request:
+                response = master.send_data_request(user_msg)
+                print('data response: {}'.format(response))
+            else:
+                response = master.send_request(user_msg)
+                print('response: {}'.format(response))
+    except KeyboardInterrupt:
+        print('Ctrl-C caught, exiting…')
+    except Exception as e:
+        print('error: {}'.format(e))
+
+if __name__ == '__main__':
+    main()
+
+#EOF
