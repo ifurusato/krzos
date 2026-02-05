33c7f0879a9397986f342a1466e7dbfa1f16cc40
ifurusato <ichiro.furusato@gmail.com>
2026-02-06 00:07:29 +1300

    copyright date update

--- ofs_test.py
+++ ofs_test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright 2020-2025 by Murray Altheim. All rights reserved. This file is part
+# Copyright 2020-2026 by Ichiro Furusato. All rights reserved. This file is part
 # of the Robot Operating System project, released under the MIT License. Please
 # see the LICENSE file included as part of this package.
 #
-# author:   Murray Altheim
+# author:   Ichiro Furusato
 # created:  2020-10-26
-# modified: 2025-02-05
+# modified: 2026-02-05
 #
 # 10 runs, average 1228 steps over 200mm, so 614 steps per 100mm or 6.14 steps/mm
 
