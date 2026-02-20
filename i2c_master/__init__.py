a96764efd873731e4281299d414faa55ef0a0dc0
ifurusato <ichiro.furusato@gmail.com>
2026-02-20 01:10:42 +0000

    added I2C master support for Radiozoa

---
+++ i2c_master/__init__.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020-2026 by Ichiro Furusato. All rights reserved. This file is part
+# of the Robot Operating System project, released under the MIT License. Please
+# see the LICENSE file included as part of this package.
+#
+# author:   Ichiro Furusato
+# created:  2025-11-16
+# modified: 2026-02-08
+
+import time
+from datetime import datetime as dt, timezone
+import smbus2
+
+from .message_util import pack_message, unpack_message
+
+class I2CMaster:
+    I2C_BUS_ID  = 1
+    I2C_ADDRESS = 0x47
+    WRITE_READ_DELAY_MS = 11
+    '''
+    I2C master controller.
+
+    NOTE: the WRITE_READ_DELAY_SEC may need adjusting for packet length and reliability.
+          If you're seeing the original command returned, that's because the master is
+          reading the memory buffer before the slave has had a chance to modify it. The
+          solution is to increase the delay time until this stops happening. Also, it
+          may help to increase the I2C baud rate.
+
+    Args:
+        i2c_id:        the I2C bus identifier (default is 1)
+        i2c_address:   the I2C device address (default is 0x47)
+    '''
+    def __init__(self, i2c_id=None, i2c_address=None, timeset=True):
+        self._i2c_bus_id  = i2c_id if i2c_id is not None else self.I2C_BUS_ID
+        self._i2c_address = i2c_address if i2c_address is not None else self.I2C_ADDRESS
+        self._enabled = False
+        self._timeset = timeset
+        self._fail_on_exception = False
+        self._delay_sec = self.WRITE_READ_DELAY_MS / 1000
+        try:
+            print('opening I2C bus {} at address {:#04x}'.format(self._i2c_bus_id, self._i2c_address))
+            self._bus = smbus2.SMBus(self._i2c_bus_id)
+            print('ready.')
+        except Exception as e:
+            print('ERROR: {} raised opening smbus: {}'.format(type(e), e))
+            raise
+
+    # ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+
+    def get_write_read_delay_ms(self):
+        '''
+        Return the current write/read delay in milliseconds.
+        '''
+        return self._delay_sec * 1000
+
+    def set_write_read_delay_ms(self, delay_ms):
+        '''
+        Override the default write/read delay to the prescribed number of milliseconds.
+        '''
+        self._delay_sec = delay_ms / 1000
+
+    def reset_write_read_delay_ms(self):
+        '''
+        Reset the default write/read delay to the default number of milliseconds.
+        '''
+        self._delay_sec = self.WRITE_READ_DELAY_MS / 1000
+
+    def set_fail_on_exception(self, fail):
+        self._fail_on_exception = fail
+
+    def _i2c_write_and_read(self, out_msg):
+        if out_msg is None:
+            raise ValueError('null message.')
+        elif len(out_msg) == 0:
+            print('WARNING: did not send empty message.')
+            return
+        # write command to register 0
+        msg_with_addr = [0x00] + list(out_msg)
+        write_msg = smbus2.i2c_msg.write(self._i2c_address, msg_with_addr)
+        self._bus.i2c_rdwr(write_msg)
+        time.sleep(self._delay_sec)
+        # write register address 0, then read
+        write_addr = smbus2.i2c_msg.write(self._i2c_address, [0x00])
+        read_msg = smbus2.i2c_msg.read(self._i2c_address, 64)
+        self._bus.i2c_rdwr(write_addr, read_msg)
+        resp_buf = list(read_msg)
+        if resp_buf and len(resp_buf) >= 2:
+            msg_len = resp_buf[0]
+            if 1 <= msg_len <= 62:
+                return bytes(resp_buf[:msg_len+2])
+        raise RuntimeError("bad message length or slave not ready.")
+
+    def send_request(self, message):
+        '''
+        send a message and return the response.
+        '''
+        if self._enabled:
+            if message.startswith('time set'):
+#               now = dt.now() # as local time
+                now = dt.now(timezone.utc) # as UTC time
+                print('setting time to: {}'.format(now.isoformat()))
+                ts = now.strftime("%Y%m%d-%H%M%S")
+                message = message.replace("now", ts)
+            out_msg = pack_message(message)
+            try:
+                resp_bytes = self._i2c_write_and_read(out_msg)
+                response = unpack_message(resp_bytes)
+                return response
+            except OSError as e:
+                raise
+            except Exception as e:
+                print('ERROR: {} raised by send request: {}'.format(type(e), e))
+                if self._fail_on_exception:
+                    raise
+                return None
+            finally:
+                # don't repeat too quickly
+                time.sleep(0.05)
+        else:
+            print('WARNING: cannot send request: disabled.')
+
+    def enable(self):
+        '''
+        enable the I2CMaster.
+        '''
+        if not self._enabled:
+            self._enabled = True
+            time.sleep(0.3)
+            if self._timeset:
+                print('setting RTC time…')
+                self.send_request('time set now')
+        else:
+            print('WARNING: already enabled.')
+
+    def disable(self):
+        '''
+        disable the I2CMaster.
+        '''
+        if self._enabled:
+            self._enabled = False
+        else:
+            print('WARNING: already disabled.')
+
+    def close(self):
+        '''
+        disable and close the I2CMaster.
+        '''
+        if not self.closed:
+            self.disable()
+            self._bus.close()
+        else:
+            print('WARNING: already closed.')
+
+#EOF
---
+++ master.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020-2026 by Ichiro Furusato. All rights reserved. This file is part
+# of the Robot Operating System project, released under the MIT License. Please
+# see the LICENSE file included as part of this package.
+#
+# author:   Ichiro Furusato
+# created:  2025-11-16
+# modified: 2026-02-13
+#
+# I2C master controller, with CLI option to set I2C address. Permits repeat
+# sending of a command using a worker thread loop initiated by "go" and halted
+# by "stop".
+
+import time
+import argparse
+from threading import Event, Lock, Thread
+from colorama import init, Fore, Style
+init()
+
+from i2c_master import I2CMaster
+from hardware.tinyfx_controller import TinyFxController
+
+# pushbutton support ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+
+import RPi.GPIO as GPIO
+
+class InterruptButton:
+    def __init__(self, pin=21, debounce_ms=50, callback=None):
+        self._pin = pin
+        self._callback = callback
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self._pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(
+            self._pin,
+            GPIO.BOTH,
+            callback=self._handle_edge,
+            bouncetime=debounce_ms
+        )
+
+    def _handle_edge(self, channel):
+        state = GPIO.input(self._pin)
+        pressed = (state == GPIO.LOW)
+        if self._callback:
+            self._callback(pressed)
+
+    def cleanup(self):
+        GPIO.remove_event_detect(self._pin)
+        GPIO.cleanup(self._pin)
+
+# ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+
+I2C_ID            = 0            # I2C bus identifier
+I2C_ADDRESS       = 0x47         # default I2C device address
+WORKER_DELAY_SEC  = 1.0          # time between automatic polls
+WORKER_REQUEST    = "distances"  # poll command
+BUTTON_DELAY_SEC  = 5            # how many seconds after pushbutton to do scan
+TICK_COMMAND      = 'play tick'
+BEEP_COMMAND      = 'play beep'
+DISTANCES_COMMAND = 'distances'
+
+def worker_loop(master, stop_event, lock):
+    '''
+    Runs until stop_event is set.
+    Repeatedly sends a request to the slave.
+    '''
+    print("worker thread started")
+
+    try:
+        while not stop_event.is_set():
+            with lock:
+                response = master.send_request(WORKER_REQUEST)
+            if response == WORKER_REQUEST:
+                print("response: {}".format(response))
+            else:
+                print("response: {}".format(response))
+            time.sleep(WORKER_DELAY_SEC)
+    finally:
+        print("worker thread stopping")
+
+#       raw = input(Fore.WHITE + "Enter x,y: " + Style.RESET_ALL).strip()
+
+def read_xy():
+    try:
+        print(Fore.WHITE + "Enter x,y: " + Style.RESET_ALL, end="", flush=True)
+        raw = input().strip()
+        x_str, y_str = raw.split(",", 1)
+        return int(x_str.strip()), int(y_str.strip())
+    except (ValueError, TypeError):
+        return None
+
+def x_read_xy():
+    try:
+        raw = input("Enter x,y: ").strip()
+        x_str, y_str = raw.split(",", 1)
+        x = int(x_str.strip())
+        y = int(y_str.strip())
+        return x, y
+    except (ValueError, TypeError):
+        return None
+
+def perform_scan(master, tfxc):
+    try:
+
+        xy = read_xy()
+        if xy is not None:
+            print(Fore.CYAN + 'xy: ' + Fore.GREEN + '{}\n'.format(xy) + Style.RESET_ALL)
+        else:
+            xy = 0,0
+            print(Fore.CYAN + 'xy: 0,0' + Style.RESET_ALL)
+
+        print(Fore.WHITE + 'continuing…' + Style.RESET_ALL)
+
+#       with lock:
+        for _ in range(BUTTON_DELAY_SEC):
+            response = tfxc.send_request(TICK_COMMAND)
+            print('response: {}'.format(response))
+            time.sleep(1)
+        response = tfxc.send_request(BEEP_COMMAND)
+        print('response: {}'.format(response))
+        time.sleep(1)
+        response = DISTANCES_COMMAND
+        while response == DISTANCES_COMMAND:
+            response = master.send_request(DISTANCES_COMMAND)
+            if response is None or response == DISTANCES_COMMAND:
+                print(Style.DIM + 'invalid response: {}'.format(response) + Style.RESET_ALL)
+            else:
+                print(Fore.CYAN + 'response: ' + Fore.GREEN + '{}'.format(response) + Style.RESET_ALL)
+
+    except Exception as e:
+        print(Fore.RED + 'ERROR: {} raised in perform_scan: {}'.format(type(e),e) + Style.RESET_ALL)
+
+
+button = None
+def main():
+
+    worker_thread = None
+    stop_event    = Event()
+    i2c_lock      = Lock()
+    i2c_address = I2C_ADDRESS
+    prompt = "► "
+
+    try:
+
+        tfxc = TinyFxController()
+        tfxc.enable()
+
+        parser = argparse.ArgumentParser(description='I2C master remote control')
+        parser.add_argument('--address',
+                type=lambda x: int(x, 0), default=I2C_ADDRESS,
+                help='I2C device address (default: 0x{:02x})'.format(I2C_ADDRESS))
+        args = parser.parse_args()
+        i2c_address = args.address
+        print('connecting to device at 0x{:02X}…'.format(i2c_address))
+        master = I2CMaster(i2c_id=I2C_ID, i2c_address=i2c_address)
+        master.enable()
+        print('\nEnter command string to send (Ctrl-C or "exit" to exit):')
+
+        def button_handler(pressed):
+            if pressed:
+                print("pressed.")
+            else:
+                print("released…")
+                perform_scan(master, tfxc)
+
+        button = InterruptButton(pin=21, debounce_ms=50, callback=button_handler)
+
+        last_user_msg = None
+        while True:
+            user_msg = input(prompt)
+
+            if not user_msg:
+                continue
+            elif last_user_msg is not None and user_msg == 'r':
+                # repeat last command
+                user_msg = last_user_msg
+            elif user_msg.strip() == 'exit' or user_msg.strip() == 'quit':
+                break
+            elif user_msg == 'go':
+                if worker_thread and worker_thread.is_alive():
+                    print("worker already running")
+                else:
+                    stop_event.clear()
+                    worker_thread = Thread(target=worker_loop, args=(master, stop_event, i2c_lock), daemon=True,)
+                    worker_thread.start()
+                continue
+            elif user_msg == 'stop':
+                if worker_thread and worker_thread.is_alive():
+                    stop_event.set()
+                    worker_thread.join()
+                else:
+                    print("worker not running")
+                continue
+
+            print('user msg: {}'.format(user_msg))
+            with i2c_lock:
+                response = master.send_request(user_msg)
+                if response == user_msg:
+                    print('🍓 response matches input.')
+#   def get_write_read_delay_ms(self):
+#   def reset_write_read_delay_ms(self):
+#   def set_write_read_delay_ms(self, delay_ms):
+
+            print('response: {}'.format(response))
+            last_user_msg = user_msg
+
+    except OSError as e:
+        print('unable to connect with device at 0x{:02X}'.format(i2c_address))
+    except KeyboardInterrupt:
+        print('Ctrl-C caught, exiting…')
+    except Exception as e:
+        print('error: {}'.format(e))
+    finally:
+        # clean shutdown
+        if worker_thread and worker_thread.is_alive():
+            stop_event.set()
+            worker_thread.join()
+
+if __name__ == '__main__':
+    main()
+
+#EOF
